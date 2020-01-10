6178e2a18f689c7f721693938f77f9b2dced13e3
Domirop <domi.trivi2000@hotmail.com>
2020-01-10 13:17:34 +0000

    login prueba

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class login extends StatelessWidget {
 
@@ -25,15 +26,99 @@ class login extends StatelessWidget {
     );
   }
 
+  Widget TextForgetPassword(){
+    return Container(
+      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+      child: GestureDetector(
+        onTap: (){
+
+        },
+        child: Text(
+          '¿Has olvidado tu contraseña?',
+          textAlign: TextAlign.right,
+          style: TextStyle(
+            color: Color.fromRGBO(0, 144, 255, 1),
+            fontWeight: FontWeight.bold,
+            decoration: TextDecoration.underline,
+            decorationColor: Color.fromRGBO(0, 144, 255, 1),
+          ),
+        ),
+      )
+    );
+  }
+
+  Widget buttonLoginIn(){
+    return Container(
+      padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+      child: RaisedButton(
+        child: Text(
+          'Entrar',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: (){
+
+        },
+      ),
+    );
+  }
+
+  Widget TextRegister(){
+    return Container(
+      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+        child: GestureDetector(
+          onTap: (){
+
+          },
+          child: Text(
+            'Registrarse',
+            textAlign: TextAlign.right,
+            style: TextStyle(
+              color: Color.fromRGBO(0, 144, 255, 1),
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
+              decorationColor: Color.fromRGBO(0, 144, 255, 1),
+            ),
+          ),
+        )
+    );
+  }
+
+  Widget lineDivisor(){
+    return Container(
+      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+      child: Row(
+        children: [
+          Divider(),
+          Text(
+              '0'
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: ExactAssetImage('assets/images/Login_6.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: ListView(
           children: <Widget>[
             emailTextField(),
             passWordTextField(),
+            TextForgetPassword(),
+            buttonLoginIn(),
+            lineDivisor(),
+
           ],
         ),
       ),
