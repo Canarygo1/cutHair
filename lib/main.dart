033d38908f884815fb88e5586f0b2b12ecc58e63
Domirop <59377864+Domirop@users.noreply.github.com>
2020-03-17 13:08:13 +0000

    Update main.dart
    ayuda

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
-import 'package:cuthair/calendarBoss.dart';
-import 'package:cuthair/chooseCalendarBoss.dart';
-import 'package:cuthair/home.dart';
+import 'package:cuthair/ui/bottom_navigation/menu.dart';
+import 'package:cuthair/ui/calendar_boss/calendar_boss.dart';
+import 'package:cuthair/ui/calendar_employee/calendar_employee.dart';
+import 'package:cuthair/ui/choose_hairdresser/choose_hairdresser.dart';
+import 'package:cuthair/ui/detail/detail_screen.dart';
+import 'package:cuthair/ui/home/home.dart';
+import 'package:cuthair/ui/home_boss/home_boss.dart';
+import 'package:cuthair/ui/login/login.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -15,7 +20,7 @@ class MyApp extends StatelessWidget {
         dividerColor: Colors.black,
         primarySwatch: Colors.blue,
       ),
-      home: CalendarBoss(),
+      home: login(),
     );
   }
 }
