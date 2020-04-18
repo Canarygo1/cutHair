e56f39c9b3e722378076de128c78cc4e873c80f8
Domirop <domi.trivi2000@hotmail.com>
2020-04-18 18:22:04 +0100

    Future: new Code.

---
+++ lib/model/schedule.dart
@@ -0,0 +1,16 @@
+import 'day.dart';
+
+class Schedule{
+  DateTime uid;
+  List disponibility;
+  List ranges;
+
+  Schedule(this.uid, this.disponibility, this.ranges);
+
+  factory Schedule.fromMap(Map values, String uid) {
+    List disponibility = values["disponibilidad"];
+    List range =  values["turnos"];
+
+    return Schedule(DateTime.parse(uid), disponibility, range);
+  }
+}
