68b3b698f6d857085a519c026b4cd32ba3477854
Alejandro <alejandro.cruz@atani.com>
2020-05-10 18:29:26 +0100

    feature:login random image

--- lib/ui/Pages/login/login.dart
+++ lib/ui/Pages/login/login.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cuthair/ui/Components/textTypes/text_error.dart';
 import 'package:cuthair/ui/Pages/register/register.dart';
 import 'package:flutter/material.dart';
@@ -20,10 +22,19 @@ class _LoginState extends State<Login> implements LoginView {
   LoginCode loginCode;
   double HEIGHT;
   double WIDHT;
-
+  List list;
+  Random rnd;
+  List<String> backgroundImages = ["assets/images/hairdressingBg.jpg","assets/images/clubBg.jpg","assets/images/restaurantBg.jpg"];
+  int randomBackground ;
   @override
   void initState() {
+
+    rnd = new Random();
+    randomBackground = 0 + rnd.nextInt(3 - 0);
+    print(randomBackground);
     loginCode = LoginCode(this);
+
+
   }
 
   @override
@@ -39,23 +50,34 @@ class _LoginState extends State<Login> implements LoginView {
           child: Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: ExactAssetImage('assets/images/Login_6.jpg'),
+                image: ExactAssetImage(backgroundImages[randomBackground]),
                 fit: BoxFit.cover,
               ),
             ),
             child: ListView(
               children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(WIDHT * 0.200,HEIGHT * 0.126,
+                      WIDHT * 0.200, 0),
+                  child: Container(
+                      child: Image.asset("assets/images/logo.png",
+                          fit: BoxFit.cover)),
+                ),
                 emailTextField(context),
                 passWordTextField(),
                 error.length == 0 ? Container() : TextError(error),
                 TextForgetPassword(context),
-                MyButton(() => logIn(), LargeText("Entrar"), color: Color.fromRGBO(230, 73, 90, 1)),
+                MyButton(() => logIn(), LargeText("Entrar"),
+                    color: Color.fromRGBO(230, 73, 90, 1)),
                 TextRegister(context),
               ],
             ),
           ),
         ));
   }
+
+  Widget logo(BuildContext context) {}
+
   Widget TextRegister(BuildContext context) {
     return Align(
         alignment: Alignment.centerRight,
@@ -77,25 +99,27 @@ class _LoginState extends State<Login> implements LoginView {
               ),
             )));
   }
+
   Widget emailTextField(BuildContext context) {
     return Container(
       child: Padding(
-        padding: EdgeInsets.fromLTRB(WIDHT * 0.101, HEIGHT * 0.176, WIDHT * 0.089, HEIGHT * 0.027),
+        padding: EdgeInsets.fromLTRB(
+            WIDHT * 0.101, HEIGHT * 0.05, WIDHT * 0.089, HEIGHT * 0.027),
         child: TextFormField(
           enableInteractiveSelection: false,
           controller: emailController,
           decoration: InputDecoration(
             hintText: 'Correo Electronico',
             enabledBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.black, width: WIDHT * 0.003),
+              borderSide: BorderSide(color: Colors.white, width: WIDHT * 0.003),
             ),
             hintStyle: TextStyle(
-              color: Colors.black,
+              color: Colors.white,
               fontSize: 18.0,
             ),
           ),
           style: TextStyle(
-            color: Colors.black,
+            color: Colors.white,
             fontSize: 18.0,
           ),
         ),
@@ -105,23 +129,24 @@ class _LoginState extends State<Login> implements LoginView {
 
   Widget passWordTextField() {
     return Padding(
-        padding: EdgeInsets.fromLTRB(WIDHT * 0.101, 0.0, WIDHT * 0.089, HEIGHT * 0.027),
+        padding: EdgeInsets.fromLTRB(
+            WIDHT * 0.101, 0.0, WIDHT * 0.089, HEIGHT * 0.027),
         child: TextFormField(
           enableInteractiveSelection: false,
           controller: passwordController,
           decoration: InputDecoration(
             hintText: 'Contraseña',
             enabledBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.black, width: WIDHT * 0.003),
+              borderSide: BorderSide(color: Colors.white, width: WIDHT * 0.003),
             ),
             hintStyle: TextStyle(
-              color: Colors.black,
+              color: Colors.white,
               fontSize: 18.0,
             ),
           ),
           obscureText: true,
           style: TextStyle(
-            color: Colors.black,
+            color: Colors.white,
           ),
         ));
   }
@@ -130,8 +155,8 @@ class _LoginState extends State<Login> implements LoginView {
     return Align(
         alignment: Alignment.centerRight,
         child: Padding(
-            padding: EdgeInsets.only(
-                bottom: HEIGHT * 0.027, right: WIDHT * 0.10),
+            padding:
+                EdgeInsets.only(bottom: HEIGHT * 0.027, right: WIDHT * 0.10),
             child: GestureDetector(
               onTap: () {
                 GlobalMethods().pushPage(context, ResetPassword());
