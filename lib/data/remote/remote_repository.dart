df5e45c6dfc4e83006ecd4fd7c29270ec972f761
Domirop <domi.trivi2000@hotmail.com>
2020-06-12 18:40:13 +0100

    feature: added filters.

--- lib/data/remote/remote_repository.dart
+++ lib/data/remote/remote_repository.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cuthair/model/appointment.dart';
 import 'package:cuthair/model/employee.dart';
 import 'package:cuthair/model/my_appointment.dart';
@@ -7,10 +8,12 @@ import 'package:cuthair/model/business.dart';
 import 'package:cuthair/model/user.dart';
 
 abstract class RemoteRepository {
+  Future<List<String>> getBusiness(Map<String, List> selectedFilters);
 
-  Future <List<String>> getBusiness();
+  Future<Map<String, List<Business>>> getAllBusiness(
+      Map<String, List> selectedFilters, List<QuerySnapshot> querySnapshots);
 
-  Future<Map<String, List<Business>>> getAllBusiness(String business);
+  Future<List<QuerySnapshot>> getAllQuery(Map<String, List> selectedFilters);
 
   Future<List<Service>> getAllServices(String uid, String typeBusiness);
 
@@ -18,25 +21,30 @@ abstract class RemoteRepository {
 
   Future<User> getUser(String uid);
 
-  Future<String> getOneImage(String businessUid, String employeeName, String directory);
+  Future<String> getOneImage(
+      String businessUid, String employeeName, String directory);
 
   Future<List<String>> getAllImages(Business business);
 
-  Future<bool> insertAppointmentHairDressing(Appointment appointment,String uid);
+  Future<bool> insertAppointmentHairDressing(
+      Appointment appointment, String uid);
 
   Future<bool> insertAppointmentRestaurant(Appointment appointment, String uid);
 
-  Future<List<MyAppointment>> getUserAppointments(String uid, DateTime date, bool firstTime);
-  
+  Future<List<MyAppointment>> getUserAppointments(
+      String uid, DateTime date, bool firstTime);
+
   Future<bool> insertAppointmentBeach(Appointment appointment, String uid);
 
   Future<User> getUserByPhoneNumber(String phoneNumber);
 
   Future<User> insertAnonymousUser(User user);
 
-  Future<Schedule> getRange(String day, String name, String businessUid, String typeBusiness);
+  Future<Schedule> getRange(
+      String day, String name, String businessUid, String typeBusiness);
 
-  Future<bool> removeRange(DateTime day, String name, String businessUid, String typeBusiness, Map ranges);
+  Future<bool> removeRange(DateTime day, String name, String businessUid,
+      String typeBusiness, Map ranges);
 
   Future<bool> removeAppointment(MyAppointment appointment);
 
