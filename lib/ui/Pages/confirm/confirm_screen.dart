1799bb8ed6cfa8c2ac234d20deee3dd4d58db26a
Domirop <domi.trivi2000@hotmail.com>
2020-06-19 12:33:59 +0100

    refactor: Change penalization.

--- lib/ui/Pages/confirm/confirm_screen.dart
+++ lib/ui/Pages/confirm/confirm_screen.dart
@@ -5,8 +5,8 @@ import 'package:cuthair/data/remote/check_connection.dart';
 import 'package:cuthair/data/remote/http_remote_repository.dart';
 import 'package:cuthair/data/remote/remote_repository.dart';
 import 'package:cuthair/global_methods.dart';
-import 'package:cuthair/model/user.dart';
 import 'package:cuthair/model/appointment.dart';
+import 'package:cuthair/model/user.dart';
 import 'package:cuthair/ui/Pages/confirm/confirm_presenter.dart';
 import 'package:cuthair/ui/Pages/confirm_animation/confirm_animation.dart';
 import 'package:flutter/cupertino.dart';
@@ -46,18 +46,16 @@ class _ConfirmScreenState extends State<ConfirmScreen> implements ConfirmView {
     WIDHT = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Color.fromRGBO(300, 300, 300, 1),
-      body: SingleChildScrollView(
-        child: getConfirmScreen()
-      ),
+      body: SingleChildScrollView(child: getConfirmScreen()),
     );
   }
 
-  Widget getConfirmScreen(){
-    if(appointment.business.typeBusiness == "Peluquerías"){
+  Widget getConfirmScreen() {
+    if (appointment.business.typeBusiness == "Peluquerías") {
       return confirmHairDressing();
-    }else if(appointment.business.typeBusiness == "Restaurantes"){
+    } else if (appointment.business.typeBusiness == "Restaurantes") {
       return confirmRestaurant();
-    }else{
+    } else {
       return confirmBeach();
     }
   }
@@ -89,7 +87,8 @@ class _ConfirmScreenState extends State<ConfirmScreen> implements ConfirmView {
             children: <Widget>[
               Expanded(child: Components.mediumText("Día: ")),
               Expanded(
-                child: Components.mediumText(appointment.checkIn.day.toString()),
+                child:
+                    Components.mediumText(appointment.checkIn.day.toString()),
               )
             ],
           ),
@@ -101,10 +100,11 @@ class _ConfirmScreenState extends State<ConfirmScreen> implements ConfirmView {
             children: <Widget>[
               Expanded(child: Components.mediumText("Hora: ")),
               Expanded(
-                child: Components.mediumText(appointment.checkIn.hour.toString() +
-                    ":" +
-                    GetTimeSeparated.getFullTimeIfHasOneValue_Hour(
-                        appointment.checkIn.minute.toString())),
+                child: Components.mediumText(
+                    appointment.checkIn.hour.toString() +
+                        ":" +
+                        GetTimeSeparated.getFullTimeIfHasOneValue_Hour(
+                            appointment.checkIn.minute.toString())),
               )
             ],
           ),
@@ -138,21 +138,23 @@ class _ConfirmScreenState extends State<ConfirmScreen> implements ConfirmView {
             children: <Widget>[
               Expanded(child: Components.mediumText("Precio cita: ")),
               Expanded(
-                  child:
-                  Components.mediumText(appointment.service.price.toString() + "€")),
+                  child: Components.mediumText(
+                      appointment.service.price.toString() + "€")),
             ],
           ),
         ),
-        (this.penalize == false || this.penalize == null) && this.appointment.business.uid == "PR01"
-            ? Container()
-            : Container(
-                padding: EdgeInsets.fromLTRB(
-                    WIDHT * 0.089, HEIGHT * 0.013, WIDHT * 0.089, 0.0),
-                child: Components.mediumText(
-                  'Existe una penalización hacia usted en este negocio, pueden haber cambios en el precio final.',
-                  color: Colors.orange,
-                ),
-              ),
+        this.appointment.business.uid == 'PR01'
+            ? this.penalize == false || this.penalize == null
+                ? Container()
+                : Container(
+                    padding: EdgeInsets.fromLTRB(
+                        WIDHT * 0.089, HEIGHT * 0.013, WIDHT * 0.089, 0.0),
+                    child: Components.mediumText(
+                      'Existe una penalización hacia usted en este negocio, pueden haber cambios en el precio final.',
+                      color: Colors.orange,
+                    ),
+                  )
+            : Container(),
         Container(
           padding: EdgeInsets.fromLTRB(
               WIDHT * 0.025, HEIGHT * 0.04, WIDHT * 0.025, 0),
@@ -169,159 +171,156 @@ class _ConfirmScreenState extends State<ConfirmScreen> implements ConfirmView {
   }
 
   Widget confirmRestaurant() {
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-              padding: EdgeInsets.fromLTRB(
-                  WIDHT * 0.05, HEIGHT * 0.135, WIDHT * 0.089, HEIGHT * 0.027),
-              child: Components.largeText("¿Desea confirmar la reserva?")),
-          Container(
-            padding: EdgeInsets.fromLTRB(
-                WIDHT * 0.254, HEIGHT * 0.04, WIDHT * 0.089, HEIGHT * 0.027),
-            child: Row(
-              children: <Widget>[
-                Expanded(child: Components.mediumText("Restaurante: ")),
-                Expanded(
-                  child: Components.mediumText(appointment.business.name),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(
-                WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
-            child: Row(
-              children: <Widget>[
-                Expanded(child: Components.mediumText("Personas: ")),
-                Expanded(
-                  child: Components.mediumText(appointment.numberPersons),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(
-                WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
-            child: Row(
-              children: <Widget>[
-                Expanded(child: Components.mediumText("Día: ")),
-                Expanded(
-                  child: Components.mediumText(appointment.checkIn.day.toString()),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(
-                WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
-            child: Row(
-              children: <Widget>[
-                Expanded(child: Components.mediumText("Hora: ")),
-                Expanded(
-                  child: Components.mediumText(appointment.checkIn.hour.toString() +
-                      ":" +
-                      GetTimeSeparated.getFullTimeIfHasOneValue_Hour(
-                          appointment.checkIn.minute.toString())),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(
-                WIDHT * 0.025, HEIGHT * 0.04, WIDHT * 0.025, 0),
-            margin: EdgeInsets.symmetric(horizontal: WIDHT * 0.025),
-            child: Row(
-              children: <Widget>[
-                buttonCancel(),
-                buttonConfirm(),
-              ],
-            ),
-          )
-        ]);
+    return Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
+        Widget>[
+      Container(
+          padding: EdgeInsets.fromLTRB(
+              WIDHT * 0.05, HEIGHT * 0.135, WIDHT * 0.089, HEIGHT * 0.027),
+          child: Components.largeText("¿Desea confirmar la reserva?")),
+      Container(
+        padding: EdgeInsets.fromLTRB(
+            WIDHT * 0.254, HEIGHT * 0.04, WIDHT * 0.089, HEIGHT * 0.027),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Components.mediumText("Restaurante: ")),
+            Expanded(
+              child: Components.mediumText(appointment.business.name),
+            )
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.fromLTRB(
+            WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Components.mediumText("Personas: ")),
+            Expanded(
+              child: Components.mediumText(appointment.numberPersons),
+            )
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.fromLTRB(
+            WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Components.mediumText("Día: ")),
+            Expanded(
+              child: Components.mediumText(appointment.checkIn.day.toString()),
+            )
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.fromLTRB(
+            WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Components.mediumText("Hora: ")),
+            Expanded(
+              child: Components.mediumText(appointment.checkIn.hour.toString() +
+                  ":" +
+                  GetTimeSeparated.getFullTimeIfHasOneValue_Hour(
+                      appointment.checkIn.minute.toString())),
+            )
+          ],
+        ),
+      ),
+      Container(
+        padding:
+            EdgeInsets.fromLTRB(WIDHT * 0.025, HEIGHT * 0.04, WIDHT * 0.025, 0),
+        margin: EdgeInsets.symmetric(horizontal: WIDHT * 0.025),
+        child: Row(
+          children: <Widget>[
+            buttonCancel(),
+            buttonConfirm(),
+          ],
+        ),
+      )
+    ]);
   }
 
-  Widget confirmBeach(){
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-              padding: EdgeInsets.fromLTRB(
-                  WIDHT * 0.05, HEIGHT * 0.135, WIDHT * 0.089, HEIGHT * 0.027),
-              child: Components.largeText("¿Desea confirmar la reserva?")),
-          Container(
-            padding: EdgeInsets.fromLTRB(
-                WIDHT * 0.254, HEIGHT * 0.04, WIDHT * 0.089, HEIGHT * 0.027),
-            child: Row(
-              children: <Widget>[
-                Expanded(child: Components.mediumText("Playa: ")),
-                Expanded(
-                  child: Components.mediumText(appointment.business.name),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(
-                WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
-            child: Row(
-              children: <Widget>[
-                Expanded(child: Components.mediumText("Personas: ")),
-                Expanded(
-                  child: Components.mediumText(appointment.numberPersons),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(
-                WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
-            child: Row(
-              children: <Widget>[
-                Expanded(child: Components.mediumText("Día: ")),
-                Expanded(
-                  child: Components.mediumText(appointment.checkIn.day.toString()),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(
-                WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
-            child: Row(
-              children: <Widget>[
-                Expanded(child: Components.mediumText("Hora: ")),
-                Expanded(
-                  child: Components.mediumText(appointment.checkIn.hour.toString() +
-                      ":" +
-                      GetTimeSeparated.getFullTimeIfHasOneValue_Hour(
-                          appointment.checkIn.minute.toString())),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(
-                WIDHT * 0.025, HEIGHT * 0.04, WIDHT * 0.025, 0),
-            margin: EdgeInsets.symmetric(horizontal: WIDHT * 0.025),
-            child: Row(
-              children: <Widget>[
-                buttonCancel(),
-                buttonConfirm(),
-              ],
-            ),
-          )
-        ]);
-
+  Widget confirmBeach() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
+        Widget>[
+      Container(
+          padding: EdgeInsets.fromLTRB(
+              WIDHT * 0.05, HEIGHT * 0.135, WIDHT * 0.089, HEIGHT * 0.027),
+          child: Components.largeText("¿Desea confirmar la reserva?")),
+      Container(
+        padding: EdgeInsets.fromLTRB(
+            WIDHT * 0.254, HEIGHT * 0.04, WIDHT * 0.089, HEIGHT * 0.027),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Components.mediumText("Playa: ")),
+            Expanded(
+              child: Components.mediumText(appointment.business.name),
+            )
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.fromLTRB(
+            WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Components.mediumText("Personas: ")),
+            Expanded(
+              child: Components.mediumText(appointment.numberPersons),
+            )
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.fromLTRB(
+            WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Components.mediumText("Día: ")),
+            Expanded(
+              child: Components.mediumText(appointment.checkIn.day.toString()),
+            )
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.fromLTRB(
+            WIDHT * 0.254, HEIGHT * 0.013, WIDHT * 0.089, HEIGHT * 0.027),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Components.mediumText("Hora: ")),
+            Expanded(
+              child: Components.mediumText(appointment.checkIn.hour.toString() +
+                  ":" +
+                  GetTimeSeparated.getFullTimeIfHasOneValue_Hour(
+                      appointment.checkIn.minute.toString())),
+            )
+          ],
+        ),
+      ),
+      Container(
+        padding:
+            EdgeInsets.fromLTRB(WIDHT * 0.025, HEIGHT * 0.04, WIDHT * 0.025, 0),
+        margin: EdgeInsets.symmetric(horizontal: WIDHT * 0.025),
+        child: Row(
+          children: <Widget>[
+            buttonCancel(),
+            buttonConfirm(),
+          ],
+        ),
+      )
+    ]);
   }
 
-  Widget buttonConfirm(){
+  Widget buttonConfirm() {
     return Expanded(
       child: Components.smallButton(
-            () => {
+        () => {
           ConnectionChecked.checkInternetConnectivity(context),
-          GlobalMethods().pushAndReplacement(
-              context, ConfirmAnimation(appointment))
+          GlobalMethods()
+              .pushAndReplacement(context, ConfirmAnimation(appointment))
         },
         Components.largeText("Confirmar"),
         horizontalPadding: WIDHT * 0.025,
@@ -329,13 +328,12 @@ class _ConfirmScreenState extends State<ConfirmScreen> implements ConfirmView {
         height: HEIGHT * 0.067,
       ),
     );
-
   }
 
-  Widget buttonCancel(){
+  Widget buttonCancel() {
     return Expanded(
       child: Components.smallButton(
-            () => {
+        () => {
           ConnectionChecked.checkInternetConnectivity(context),
           GlobalMethods().removePages(context)
         },
